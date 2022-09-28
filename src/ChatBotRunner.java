dacf53a29ce3bab2137909539aa8edbd2e2b6df4
1E10 Laptop <sample@bths.edu>
2022-09-28 10:56:55 -0400

    V2- Lab Completed Up to freestyle

---
+++ src/ChatBotRunner.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ChatBotRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ChatBot chatbot1 = new ChatBot("Saul Goodman", 5);
+        chatbot1.greeting("Rajit"); //Calls Method 1
+        chatbot1.weather(); //Calls method 2
+
+        System.out.println("Give me a random height to the nearest foot, I'll convert it to meters!");
+        int feetInput = scan.nextInt();
+        scan.nextLine();
+        System.out.println(feetInput + " feet to meters is: " + chatbot1.convertFeetToMeters(feetInput)); //Calls Method 3
+        System.out.println("Hey what's your favorite whole number?");
+        int userFavoriteNumber = scan.nextInt();
+        chatbot1.favoriteNumber(userFavoriteNumber); //Calls method 4
+        System.out.println("Hey! Give me 3 random whole numbers, will ya?");
+        int num1Input = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Alright 2 more! Let's see what you got.");
+        int num2Input = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Its the final number! Let's see what you have in store for me.");
+        int num3Input = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Would you believe me if I told you I can do something crazy?"); //Calls Method 5
+        System.out.println("The sum of those 3 numbers is.. " + chatbot1.addNumbers(num1Input, num2Input, num3Input) + "!! Crazy isn't it?");
+        System.out.println(chatbot1.goodbye()); //Calls Method 6
+
+
+
+
+
+    }
+}
--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,10 +1,10 @@
 public class CircleRunner {
     public static void main(String[] args) {
         // A. create a Circle object with radius 5.0
-        Circle circ1 = new Circle(5.0);
+        Circle Circle  = new Circle(5.0);
         // B. locate the method defined in the Circle class and write
         //    a line of code to call it on your object
-        circ1.printArea();
+        Circle.printArea();
         // C. BEFORE running this code, PREDICT what gets printed
             //My area is 78.5//
         // D. run the code and confirm your prediction! Were you correct?
